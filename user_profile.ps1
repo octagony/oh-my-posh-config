5e2cbcffe449cac7c799121e496de2bde9cfa647
Octagony <moctagony@gmail.com>
2022-08-27 18:15:41 +0300

    [Init]

---
+++ user_profile.ps1
@@ -0,0 +1,22 @@
+#Prompt
+Import-Module posh-git
+Import-Module Terminal-Icons
+oh-my-posh.exe init pwsh --config 'C:\Users\Octagony\.config\powershell\stelbnet.omp.json' | Invoke-Expression
+
+# PSReadLine
+Set-PSReadlineOption -EditMode Emacs
+Set-PSReadlineOption -BellStyle None
+Set-PSReadlineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+
+#Allias
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias grep findstr
+Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+
+#Utilites
+function which($command){
+	Get-Command -Name $command -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
